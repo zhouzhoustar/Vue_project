0fe30f57fe314557f1dc7c978c493a4c62104d13
zhouzhoustar <690571152@qq.com>
2019-04-19 23:04:27 +0800

    跨域成功

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    devServer: {
+      proxy: {
+        '/api': {
+            target: "http://localhost:3000",
+            ws: true,
+          changeOrigin: true,
+          pathRewrite: {
+            "^/api" : ""
+          }
+        }
+         
+      }
+    }
+  }
